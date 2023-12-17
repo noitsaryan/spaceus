b35afdfeb62e188b62856a5388d0e514ed3550b9
Aryan Vishwakarma <aryanongithub@gmail.com>
2023-12-17 14:38:06 +0530

    Test

--- components/HomeForm.tsx
+++ components/HomeForm.tsx
@@ -9,7 +9,7 @@ function HomeForm() {
             <Button name='Get Free Quote' url='/get-free-quote?type=residence&bhk=1rk&area=250'
                 style='text-white'
             />
-                <input type="number"   className='text-lg py-2 px-4 rounded-md text-orange-600' placeholder='(+91) Enter Mobile Nunber' />
+                <input type="number" name='number'  className='text-lg py-2 px-4 rounded-md text-orange-600' placeholder='(+91) Enter Mobile Nunber' />
             </form>
         </div>
     )
